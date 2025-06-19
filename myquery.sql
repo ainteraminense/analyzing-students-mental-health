2e21e2af4519e60a09abbb54e4bf3d1819ec75a5
ainteraminense <99498988+ainteraminense@users.noreply.github.com>
2025-06-18 20:05:52 -0400

    Add the 5 columns grouped by stay

--- myquery.sql
+++ myquery.sql
@@ -1,3 +1,10 @@
-SELECT *
+SELECT stay,
+       COUNT(stay) AS count_int,
+       ROUND(AVG(todep), 2) AS average_phq,
+       ROUND(AVG(tosc), 2) AS average_scs,
+       ROUND(AVG(toas), 2) AS average_as
 FROM students
-LIMIT 5;
+GROUP BY stay
+ORDER BY stay DESC
+LIMIT 9;
+
